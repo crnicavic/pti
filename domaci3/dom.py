35e6f7b6a57ecce8efa1f934f9dae6fa86cc591d
crnicavic <cavic5000@tutanota.com>
2024-12-06 23:14:58 +0100

    actually works pretty good even with a lot of variance

--- domaci3/dom.py
+++ domaci3/dom.py
@@ -1,23 +1,39 @@
-import random
+import numpy.random as rnd
 
-agent_graph = {
+default_agent_graph = {
 	0: [1],
 	1: [0, 2, 3, 4],
 	2: [0, 3],
 	3: [1, 2],
-	4: [1]}
+	4: [1]
+}
+
+def consensus(value, alpha, iter_count, var, eps, agent_graph):
+	if agent_graph is None:
+		agent_graph = default_agent_graph
+	
+	agent_vals = (rnd.rand(len(agent_graph)) + var) * value
+
+	for i in range(iter_count):
+		for agent in agent_graph:
+			gamma = 1 / len(agent_graph[agent])
+			u = 0
+			for adj in agent_graph[agent]:
+				u += gamma * (agent_vals[adj] - agent_vals[agent])
+			
+			# new measurement
+			new_m = rnd.uniform(VALUE*0.5, 1.5*VALUE)
+			b = (new_m - agent_vals[agent]) * alpha
+
+			agent_vals[agent] += b + u
+
+	return agent_vals
+
 
 VALUE = 69
 
-gamma = 0.01
-
+alpha = 0.01
 ITERATIONS = 1000
-agent_vals = [random.uniform(VALUE*0.8, 1.2*VALUE) for _ in range(len(agent_graph))]
 
-for _ in range(ITERATIONS):
-	for agent in agent_graph:
-		for adj in agent_graph[agent]:
-			agent_vals[agent] += gamma * (agent_vals[adj] - agent_vals[agent])
-
-print(agent_vals)
-	
+vals = consensus(VALUE, alpha, ITERATIONS, 0.5, 1e-3, None)	
+print(vals)
